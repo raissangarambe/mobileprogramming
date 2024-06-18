8b2df28282f525c5f35939ef10449b9080825136
Raissa <raissangr1012@gmail.com>
2024-06-18 14:23:05 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _CalculatorState extends State<Calculator> {
     return Container(
       child: ElevatedButton(
         onPressed: () {
-          calculation(btntxt);
+          // The onPressed function is intentionally left blank
         },
         child: Text(
           btntxt,
@@ -65,7 +65,7 @@ class _CalculatorState extends State<Calculator> {
                   Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Text(
-                      '$text',
+                      '0', // Static text for display
                       textAlign: TextAlign.left,
                       style: TextStyle(
                         color: Colors.white,
@@ -126,7 +126,7 @@ class _CalculatorState extends State<Calculator> {
                     backgroundColor: Colors.grey[850],
                   ),
                   onPressed: () {
-                    calculation('0');
+                    // The onPressed function is intentionally left blank
                   },
                   child: Text(
                     '0',
@@ -145,106 +145,5 @@ class _CalculatorState extends State<Calculator> {
         ),
       ),
     );
-  }
-
-  // Calculator logic goes here
-  dynamic text = '0';
-  double numOne = 0;
-  double numTwo = 0;
-
-  dynamic result = '';
-  dynamic finalResult = '0';
-  dynamic opr = '';
-  dynamic preOpr = '';
-
-  void calculation(String btnText) {
-    if (btnText == 'AC') {
-      text = '0';
-      numOne = 0;
-      numTwo = 0;
-      result = '';
-      finalResult = '0';
-      opr = '';
-      preOpr = '';
-    } else if (opr == '=' && btnText == '=') {
-      if (preOpr == '+') {
-        finalResult = add();
-      } else if (preOpr == '-') {
-        finalResult = sub();
-      } else if (preOpr == 'x') {
-        finalResult = mul();
-      } else if (preOpr == '/') {
-        finalResult = div();
-      }
-    } else if (btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
-      if (numOne == 0) {
-        numOne = double.parse(result);
-      } else {
-        numTwo = double.parse(result);
-      }
-
-      if (opr == '+') {
-        finalResult = add();
-      } else if (opr == '-') {
-        finalResult = sub();
-      } else if (opr == 'x') {
-        finalResult = mul();
-      } else if (opr == '/') {
-        finalResult = div();
-      }
-      preOpr = opr;
-      opr = btnText;
-      result = '';
-    } else if (btnText == '%') {
-      result = (numOne / 100).toString();
-      finalResult = doesContainDecimal(result);
-    } else if (btnText == '.') {
-      if (!result.toString().contains('.')) {
-        result = result.toString() + '.';
-      }
-      finalResult = result;
-    } else if (btnText == '+/-') {
-      result = result.toString().startsWith('-') ? result.toString().substring(1) : '-' + result.toString();
-      finalResult = result;
-    } else {
-      result = result + btnText;
-      finalResult = result;
-    }
-
-    setState(() {
-      text = finalResult;
-    });
-  }
-
-  String add() {
-    result = (numOne + numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String sub() {
-    result = (numOne - numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String mul() {
-    result = (numOne * numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String div() {
-    result = (numOne / numTwo).toString();
-    numOne = double.parse(result);
-    return doesContainDecimal(result);
-  }
-
-  String doesContainDecimal(dynamic result) {
-    if (result.toString().contains('.')) {
-      List<String> splitDecimal = result.toString().split('.');
-      if (!(int.parse(splitDecimal[1]) > 0)) return result = splitDecimal[0].toString();
-    }
-    return result;
   }
 }
